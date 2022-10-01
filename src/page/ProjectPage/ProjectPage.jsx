17918b80d0e6bbba10b09b80cd3af53039775e41
zPavel391 <golubev33108ai@gmail.com>
2022-10-01 23:52:02 +0200

    fix: project description

--- src/page/ProjectPage/ProjectPage.jsx
+++ src/page/ProjectPage/ProjectPage.jsx
@@ -8,27 +8,20 @@ export const ProjectPage = () => {
             <div className="demonstration__list">
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        Проект для моего друга, рекламный сайт (html + css + JQery)
+                        Адаптивная верстка, рекламный сайт для моего знакомого (html + css + JQery(слайдер))
                     </p>
                     <a className="demonstration__link" href="https://obrabotka39kld.ru/">https://obrabotka39kld.ru</a>
                 </div>
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        Верстка макета
-                    </p>
-                    <a className="demonstration__link"
-                        href="https://vercel.com/zpavel39/web-site-practice">web-site-practice</a>
-                </div>
-                <div className='demonstration__section'>
-                    <p className="demonstration__info">
-                        Верстка экрана входа и регистрации, реализована валидация при регистарции.
+                        Верстка формы входа и регистрации, реализована валидация при регистарции (на чистом JS лендинг перенесен на React).
                     </p>
                     <a className="demonstration__link"
                         href="https://web-form-practic-qi3skocxy-zpavel39.vercel.app/">web-form-practice</a>
                 </div>
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        Проект содежращий стек: React, Routing, Redux-Toolkit, React-redux
+                        Реализация приложения онлайн заказа рестаранной продукции. Проект содежращий стек: React, Routing, Redux-Toolkit, React-redux
                     </p>
                     <a className="demonstration__link"
                         href="https://web-form-practic-qi3skocxy-zpavel39.vercel.app/">web-form-practice</a>
