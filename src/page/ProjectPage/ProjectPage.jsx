15b7d37b857bf86155968953aaf686d3c84bd19e
zPavel391 <golubev33108ai@gmail.com>
2023-01-11 12:50:47 +0200

    fix: syntax error

--- src/page/ProjectPage/ProjectPage.jsx
+++ src/page/ProjectPage/ProjectPage.jsx
@@ -8,28 +8,28 @@ export const ProjectPage = () => {
             <div className="demonstration__list">
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        Верстка формы входа и регистрации, реализована валидация при регистарции (на чистом JS лендинг перенесен на React).
+                        Верстка формы входа и регистрации, реализована валидация при регистрации (на чистом JS лендинг перенесен на React).
                     </p>
                     <a className="demonstration__link"
                         href="https://web-form-practic-qi3skocxy-zpavel39.vercel.app/">web-form-practice</a>
                 </div>
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        Реализация приложения онлайн заказа рестаранной продукции. Проект содежращий стек: React, Routing, Redux-Toolkit, React-redux
+                        Реализация приложения онлайн заказа ресторанной продукции. Проект содержащий стек: React, Routing, Redux-Toolkit, React-redux
                     </p>
                     <a className="demonstration__link"
                         href="https://web-spa-project.vercel.app/">web-spa-project</a>
                 </div>
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                    Реализация приложения отображения списка сотрудников (поиск по имени, фамилии и тегу, сортировка, информация о каждом сотруднике при клике). Проект содежращий стек: React, Routing, Redux-Toolkit, React-redux, Axios, SCSS
+                    Реализация приложения отображения списка сотрудников (поиск по имени, фамилии и тегу, сортировка, информация о каждом сотруднике при клике). Проект содержащий стек: React, Routing, Redux-Toolkit, React-redux, Axios, SCSS
                     </p>
                     <a className="demonstration__link"
                         href="https://search-users-project.vercel.app/">search-users-project</a>
                 </div>
                 <div className='demonstration__section'>
                     <p className="demonstration__info">
-                        ToDo List c возможностью прикрепления файла и скачивания его. Проект содежращий стек: React, Firebase, Axios, LESS
+                        ToDo List c возможностью прикрепления файла и скачивания его. Проект содержащий стек: React, Firebase, Axios, LESS
                     </p>
                     <a className="demonstration__link" href="https://web-todo-project.web.app/">web-todo-project</a>
                 </div>
